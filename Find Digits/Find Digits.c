95bc19a35238c49730959ff116f7a112a280633b
rachit bhutani <rachitbhutani1998@gmail.com>
2017-03-29 00:01:47 +0530

    updated indentation

--- Find Digits/Find Digits.c
+++ Find Digits/Find Digits.c
@@ -1,27 +1,28 @@
-#include <stdio.h>  
-#include <string.h>  
-#include <math.h>  
-#include <stdlib.h>  
-int main()   
-{  
-      int T;  
-      int M,N,count,rem;  
-      scanf("%d",&T);  
-      while(T>0)  
-      {  
-           scanf("%d",&N);  
-           M=N;  
-           count=0;  
-           while(N>0)  
-           {  
-                rem=N%10;  
-                if(rem!=0)  
-                if(M%rem==0)  
-                count++;  
-                N=N/10;  
-           }  
-           printf("%d\n",count);  
-           T--;  
-      }  
-return 0;  
-}  
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+      int T;
+      int M,N,count,rem;
+      scanf("%d",&T);
+      while(T>0)
+      {
+           scanf("%d",&N);
+           M=N;
+           count=0;
+           while(N>0)
+           {
+                rem=N%10;
+                if(rem!=0)
+                if(M%rem==0)
+                count++;
+                N=N/10;
+           }
+           printf("%d\n",count);
+           T--;
+      }
+return 0;
+}
